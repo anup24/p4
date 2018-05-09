742761f1ab32657dffee573f8ed825f82e9106e6
Anup Shetye <anup.shetye@gmail.com>
2018-05-09 19:24:43 -0400

    Latest changes to p4

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,6 @@
 <head>
     <title>@yield('title', 'WikiText')</title>
     <meta charset='utf-8'>
-
     <link href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" rel="stylesheet" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
     <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>
     <link href='/css/wikitext.css' type='text/css' rel='stylesheet'>
